a62f2fe0dbbb8852289ecaf74487ee4625e45452
桑榆肖物 <sang93@qq.com>
2024-05-29 00:46:37 +0800

    test: 增加测试数据

--- test/UAParserTest.cs
+++ test/UAParserTest.cs
@@ -6,7 +6,9 @@ namespace Sang.UAParser.Test
         [InlineData("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
         "Edge","125.0.0.0","Windows","10","Desktop")]
         [InlineData("Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.4467.141 Mobile Safari/537.36",
-        "Safari","537.36","Android","11","Mobile")]
+        "Chrome","80.0.4467.141","Android","11","Mobile")]
+        [InlineData("Mozilla/3.0 (compatible; NetPositive/2.1.1; BeOS)",
+        "NetPositive","2.1.1","Other","","Bot")]
         // 添加更多的测试数据
         public void TestMyMethod(string useragent, string browser, string browserVersion, string os, string osVersion, string deviceType)
         {
